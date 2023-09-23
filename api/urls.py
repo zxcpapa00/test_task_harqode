d5df33a6a36b42f8525359908e58c0b18c81c093
Evgeniy K <pgg1337@mail.ru>
2023-09-23 13:29:52 +0300

    add comments

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,13 @@ from .views import LessonModelViewSet, ProductsViewSet, AllProductsViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
+# Вывод всех уроков по всем продуктам к которым пользователь имеет доступ
 router.register(r'lessons', LessonModelViewSet)
+
+# Вывод уроков по конкретному продукту по пути /products/<int:pk>
 router.register(r'products', ProductsViewSet)
+
+# Вывод всех продуктов с подробной информацией
 router.register(r'all-products', AllProductsViewSet)
 
 
